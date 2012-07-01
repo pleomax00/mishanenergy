672219ef69c914706edea369ad49b09a292a9f39
Shamail Tayyab <shamail@tunesdiary.com>
2012-07-01 13:42:35 +0530

    Updated DB conf

--- mishan/settings.py
+++ mishan/settings.py
@@ -8,7 +8,7 @@ TEMPLATE_DEBUG = DEBUG
 FORCE_SCRIPT_NAME = ""
 
 if MODE == "PRODUCTION":
-    PATH_ROOT = "/var/lib/stickshift/0e6de6785c1247cab0a6485bf73e657f/mishan/repo/wsgi"
+    PATH_ROOT = "/home/ubuntu/install/mishan"
 else:
     PATH_ROOT = os.getcwd()
 
@@ -24,10 +24,14 @@ if MODE == "PRODUCTION":
         'default': {
             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'mishan',                      # Or path to database file if using sqlite3.
-            'USER': 'admin',                      # Not used with sqlite3.
-            'PASSWORD': 'S2KHl2pekg19',                  # Not used with sqlite3.
-            'HOST': '127.7.153.1',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+            'USER': 'ubuntu',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            #'USER': 'admin',                      # Not used with sqlite3.
+            #'PASSWORD': 'S2KHl2pekg19',                  # Not used with sqlite3.
+            #'HOST': '127.7.153.1',                      # Set to empty string for localhost. Not used with sqlite3.
+            #'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
         }
     }
 
