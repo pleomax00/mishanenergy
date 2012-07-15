79ce076ea68fd7fc6ad5cc68db4079fa1980cda7
Shamail Tayyab <shamail@tunesdiary.com>
2012-07-15 17:00:37 +0530

    Nimmis suggestions

--- static/js/ddaccordion.js
+++ static/js/ddaccordion.js
@@ -7,7 +7,7 @@
 
 
 var ddaccordion={
-	ajaxloadingmsg: '<img src="loading2.gif" /><br />Loading Content...', //customize HTML to output while Ajax content is being fetched (if applicable)
+	ajaxloadingmsg: '<img src="/static/images/loading2.gif" /><br />Loading Content...', //customize HTML to output while Ajax content is being fetched (if applicable)
 
 	headergroup: {}, //object to store corresponding header group based on headerclass value
 	contentgroup: {}, //object to store corresponding content group based on headerclass value
